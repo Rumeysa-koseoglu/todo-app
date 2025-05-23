2a02ac8609d0eb4fe66ba2d151342633b0dbd5b9
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-05-23 14:27:26 +0300

    feat: add task creation with props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import './App.css'
-import CreateTodo from './components/CreateTodo'
-import TodoList from './components/TodoList'
+import { useState } from "react";
+import "./App.css";
+import CreateTodo from "./components/CreateTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
+  //we define a state called todos, initially  it is an empty array
+  const [todos, setTodos] = useState([]);
+
+  //we define a function to add a new task
+  const AddTask = (newTodo) => {
+    //with spread operator we add a new one to the end of the current tasks
+    setTodos([...todos, newTodo]);
+  };
+
+  console.log(todos);
 
   return (
-    <div className='app'>
-      <div style={{ width: "500px", display:"flex", flexDirection:"column", alignItems:"center",
-         justifyContent:"center"}}>
-    <CreateTodo/>
-    <TodoList/>
+    <div className="app">
+      <div
+        style={{
+          width: "500px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/**we call CreateTodo component and send it the AddTask function */}
+        <CreateTodo onAddTask={AddTask} />
+        <TodoList />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import { IoIosAddCircle } from "react-icons/io";
 
-function CreateTodo() {
+//we get onAddTask function from props object
+function CreateTodo({onAddTask}) {
+
+    //we define a state named newTask, it will hold text in input
+    const [newTask, setNewTask] =useState ('');
+
+    //a function to create a new task
+    const CreateNewTask = () => {
+        //if the input field is empty, do nothing
+        if(!newTask) return;
+        //we create a new object named 'request' to the new task (id + content)
+        const request = {
+            id : Math.floor(Math.random() * 9999999999999), //to get an id made up of random numbers
+            content: newTask //the text written by the user
+        }
+        //we send this object to the parent component
+        onAddTask(request)
+    }
+
     return (
         
             <div className='create-todo'>
-                <input className='todo-input' type="text" placeholder='enter a task' />
-                <IoIosAddCircle size='50px' color='#796b5e'/>
+                {/** the field where the user enters a task */}
+                <input value={newTask} //input value depends on state
+                onChange={(e) => setNewTask(e.target.value)} //when input changes, state is updated
+                 className='todo-input' type="text" placeholder='enter a task' />
+                <IoIosAddCircle size='50px' color='#796b5e' onClick={CreateNewTask} />
             </div>
     
     )
